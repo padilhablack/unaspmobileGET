75a282fb56435b9f8c5b02e2657d8ed626bda6af
ec.086539 <ec.086539@C2-NMAZ-02.IASD.ORG>
2014-09-19 15:42:06 -0300

    FIM DOS SQLS

--- index.php
+++ index.php
@@ -16,14 +16,79 @@ and open the template in the editor.
 
 //LOGIN 
             $(document).ready(function() {
-                
-//                $.get("http://www.unasp-ec.com/unaspserver/login.php?ra=86539&senha=9886", function(data) {
-//                    $("#result").html(data);
+
+                $("#option").change(function() {
+                    $("#option option:selected").each(function() {
+                        CURSO = $(this).val();
+                        
+                        switch (CURSO) {
+                            case 'login':
+       
+                                $.get("http://www.unasp-ec.com/unaspserver/login.php?ra=86539&senha=9886", function(data) {
+                                    $("#result").html(data);
+                                   
+                                });
+
+                                break;
+
+                            case 'curso':
+        
+                                $.get("http://www.unasp-ec.com/unaspserver/notas/curso.php?ra=86539", function(data) {
+                                    $("#result").html(data);
+                                   
+                                });
+                                break;
+                            case 'periodo':
+                        
+                                $.get("http://www.unasp-ec.com/unaspserver/notas/periodo.php?ra=86539&curso=800", function(data) {
+                                    $("#result").html(data);
+                                    
+                                });
+                                break;
+
+                            case 'turma':
+          
+                                $.get("http://www.unasp-ec.com/unaspserver/notas/turma.php?ra=86539&curso=800&ano=2014&periodo=1", function(data) {
+                                    $("#result").html(data);
+                                 
+                                });
+                                break;
+                            case 'disciplina':
+                           
+                                $.get("http://www.unasp-ec.com/unaspserver/notas/disciplina.php?ra=86539&curso=800&ano=2014&periodo=1&turma=5A800", function(data) {
+                                    $("#result").html(data);
+                                
+                                });
+                                break;
+                            case 'media':
+                         
+                                $.get("http://www.unasp-ec.com/unaspserver/notas/media.php?ra=86539&ano=2014&turma=5A800&periodo=1&disciplina=G1311", function(data) {
+                                    $("#result").html(data);
+                              
+                                });
+                                break;
+
+                            case 'financeiro':
+                           
+                                $.get("http://www.unasp-ec.com/unaspserver/financeiro/getFinanceiro.php?ra=86539", function(data) {
+                                    $("#result").html(data);
+                      
+                                });
+                            default:
+                                $.get("http://www.unasp-ec.com/unaspserver/login.php?ra=86539&senha=9886", function(data) {
+                                    $("#result").html(data);
+                                  
+                                });
+                        }
+                    });
+                });
+
+
+//LOGIN
+//            $.get("http://www.unasp-ec.com/unaspserver/login.php?ra=86539&senha=9886", function(data) {
+//            $("#result").html(data);
 //                    alert("Load was performed.");
-//                });
-
-
-
+//            });
 //CURSO
 // 
 //                $.get("http://www.unasp-ec.com/unaspserver/notas/curso.php?ra=86539", function(data) {
@@ -47,15 +112,99 @@ and open the template in the editor.
 //                });
 
 //DISCIPLINA
-                  $.get("http://www.unasp-ec.com/unaspserver/notas/disciplina.php?ra=86539&curso=800&ano=2014&periodo=1&turma=5A800", function(data) {
-                    $("#result").html(data);
-                    alert("Load was performed.");
-                });
+//                  $.get("http://www.unasp-ec.com/unaspserver/notas/disciplina.php?ra=86539&curso=800&ano=2014&periodo=1&turma=5A800", function(data) {
+//                    $("#result").html(data);
+//                    alert("Load was performed.");
+//                });
 //                
+
+//MEDIA                   
+//
+//              $.get("http://www.unasp-ec.com/unaspserver/notas/media.php?ra=86539&ano=2014&turma=5A800&periodo=1&disciplina=G1311", function(data) {
+//                    $("#result").html(data);
+//                    alert("Load was performed.");
+//                });
+
+//FINANCEIRO                   
+//
+//              $.get("http://www.unasp-ec.com/unaspserver/financeiro/getFinanceiro.php?ra=86539", function(data) {
+//                    $("#result").html(data);
+//                    alert("Load was performed.");
+//                });
+
             });
 
-
         </script>
+
+        $(document).ready(function() {
+
+        <h3>Login</h3>
+
+        <p> 
+
+
+            //                $.get("http://www.unasp-ec.com/unaspserver/login.php?ra=86539&senha=9886", function(data) {
+            //                    $("#result").html(data);
+            //                    alert("Load was performed.");
+            //                });
+        </p>
+
+
+        <h3>CURSO</h3>
+        // 
+        //                $.get("http://www.unasp-ec.com/unaspserver/notas/curso.php?ra=86539", function(data) {
+        //                    $("#result").html(data);
+        //                    alert("Load was performed.");
+        //                });
+
+
+
+        <h3>PERIODO</h3>          
+        //                $.get("http://www.unasp-ec.com/unaspserver/notas/periodo.php?ra=86539&curso=800", function(data) {
+        //                    $("#result").html(data);
+        //                    alert("Load was performed.");
+        //                });
+        //                
+
+        <h3>TURMA</h3>         
+        //                $.get("http://www.unasp-ec.com/unaspserver/notas/turma.php?ra=86539&curso=800&ano=2014&periodo=1", function(data) {
+        //                    $("#result").html(data);
+        //                    alert("Load was performed.");
+        //                });
+
+        <h3>DISCIPLINA</h3>
+        //                  $.get("http://www.unasp-ec.com/unaspserver/notas/disciplina.php?ra=86539&curso=800&ano=2014&periodo=1&turma=5A800", function(data) {
+        //                    $("#result").html(data);
+        //                    alert("Load was performed.");
+        //                });
+        //                
+
+        <h3>MEDIA</h3>                   
+        //
+        //              $.get("http://www.unasp-ec.com/unaspserver/notas/media.php?ra=86539&ano=2014&turma=5A800&periodo=1&disciplina=G1311", function(data) {
+        //                    $("#result").html(data);
+        //                    alert("Load was performed.");
+        //                });
+
+        <h3>FINANCEIRO</h3>                   
+        //
+        //              $.get("http://www.unasp-ec.com/unaspserver/financeiro/getFinanceiro.php?ra=86539", function(data) {
+        //                    $("#result").html(data);
+        //                    alert("Load was performed.");
+        //                });
+
+        <br/>
+        <select id="option">
+            <option id="login" name="login" value="login">Login</option>
+            <option id="curso" name="curso" value="curso">curso</option>
+            <option id="periodo" name="periodo" value="periodo">periodo</option>
+            <option id="turma" name="turma" value="turma">turma</option>
+            <option id="disciplina" name="disciplina" value="disciplina">disciplina</option>
+            <option id="media" name="media" value="media">media</option>
+            <option id="financeiro" name="financeiro" value="financeiro">financeiro</option>
+        </select>
         <div id="result"></div>
+
     </body>
+
 </html>
--- notas/curso.php
+++ notas/curso.php
@@ -2,27 +2,39 @@
 
 include '../classes/Database.php';
 
-$raGET = $_GET['ra'];
-//$raGET = 86539;
+//$raGET = $_GET['ra'];
+$raGET = $_GET['ra'] = 86539;
 
 $result = array('erro' => false);
 
-$sql = mssql_query("SELECT DISTINCT supervisor.CURSO.COD_CURSO, RTRIM(LTRIM(supervisor.CURSO.DES_CURSO)) AS DES_CURSO  FROM supervisor.ALUNO_X_MATRIZ_CURRICULAR INNER JOIN  supervisor.MATRIZ_CURRICULAR ON supervisor.ALUNO_X_MATRIZ_CURRICULAR.COD_MATRIZ_CURRICULAR = supervisor.MATRIZ_CURRICULAR.COD_MATRIZ_CURRICULAR INNER JOIN  supervisor.CURSO ON supervisor.MATRIZ_CURRICULAR.COD_CURSO = supervisor.CURSO.COD_CURSO INNER JOIN  supervisor.UNIDADE_ESCOLAR ON supervisor.CURSO.COD_UNIDADE_ESCOLAR = supervisor.UNIDADE_ESCOLAR.COD_UNIDADE_ESCOLAR AND ISNULL(supervisor.UNIDADE_ESCOLAR.COD_PARAMETRO,0) <> 42 WHERE (supervisor.ALUNO_X_MATRIZ_CURRICULAR.COD_PESSOA = ".$raGET.")  ORDER BY DES_CURSO");
+$sql = mssql_query("SELECT DISTINCT supervisor.CURSO.COD_CURSO, RTRIM(LTRIM(supervisor.CURSO.DES_CURSO)) AS DES_CURSO  FROM supervisor.ALUNO_X_MATRIZ_CURRICULAR INNER JOIN  supervisor.MATRIZ_CURRICULAR ON supervisor.ALUNO_X_MATRIZ_CURRICULAR.COD_MATRIZ_CURRICULAR = supervisor.MATRIZ_CURRICULAR.COD_MATRIZ_CURRICULAR INNER JOIN  supervisor.CURSO ON supervisor.MATRIZ_CURRICULAR.COD_CURSO = supervisor.CURSO.COD_CURSO INNER JOIN  supervisor.UNIDADE_ESCOLAR ON supervisor.CURSO.COD_UNIDADE_ESCOLAR = supervisor.UNIDADE_ESCOLAR.COD_UNIDADE_ESCOLAR AND ISNULL(supervisor.UNIDADE_ESCOLAR.COD_PARAMETRO,0) <> 42 WHERE (supervisor.ALUNO_X_MATRIZ_CURRICULAR.COD_PESSOA = 86539)  ORDER BY DES_CURSO");
 
 $alunos = array();
+$cont = 0;
 $mostrar = array();
 
 while ($row = mssql_fetch_assoc($sql)) {
 
-    $alunos[] = $row;
+
+    $alunos[] = array(
+        'COD_CURSO' => $row['COD_CURSO'],
+        'DES_CURSO' => $row['DES_CURSO'],
+    );
 }
 for ($i = 0; $i < count($alunos); $i++) {
-    $mostrar = array(
-        'id_curso' => $alunos[$i]['COD_CURSO'],
-        'nome_curso' => $alunos[$i]['DES_CURSO']
-    );
-    $encodedArray = array_map(utf8_decode,$mostrar);
-    $encode = json_encode($encodedArray);
+    $curso = $alunos[$i]['COD_CURSO'];
+//     echo $curso ." ";
+    $DES_CURSO = $alunos[$i]['DES_CURSO'];
+//     echo $DES_CURSO;
+//     echo '<br />';
 
+    $mostrar = array(
+        'id_curso' => $curso,
+        'nome_curso' => $DES_CURSO
+    );
+
+    $encodedArray = array_map(utf8_encode, $mostrar);
+    $encode = json_encode($encodedArray);
+    echo $encode . ', <br />';
 }
 ?>
